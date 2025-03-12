4fe1f1b31776739b2fd89fe8e4988bccb5573240
Ivona Nicetin <ivona.nicetin@gmail.com>
2025-03-12 01:09:44 -0700

    Enable/ Disable Menu pop up

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -5,17 +5,50 @@ public class GameManager : SingletonMonoBehavior<GameManager>
 {
     [SerializeField] private int score;
     [SerializeField] private CoinCounterUI coinCounter;
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private GameObject settingsMenu;
+
+    private bool isSettingsMenuActive;
+
+    public bool IsSettingsMenuActive => isSettingsMenuActive;
 
     protected override void Awake()
     {
         base.Awake();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        inputManager.OnSettingsMenu.AddListener(ToggleSettingsMenu);
+         //the gamestartswiththe settingsmenudisabled
+        DisableSettingsMenu();
     }
 
     public void IncreaseScore()
     {
         score++;
         coinCounter.UpdateScore(score);
+    }
+
+    private void ToggleSettingsMenu()
+    {
+        if (isSettingsMenuActive) DisableSettingsMenu();
+        else EnableSettingsMenu();
+    }
+
+    private void EnableSettingsMenu()
+    {
+        Time.timeScale = 0f;
+        settingsMenu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isSettingsMenuActive = true;
+    }
+
+    private void DisableSettingsMenu()
+    {
+        Time.timeScale = 1f;
+        settingsMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isSettingsMenuActive = false;
     }
 }
